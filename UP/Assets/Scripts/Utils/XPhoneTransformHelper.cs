936f457a082bfa3ab404c02aa0d069eac265a7a8
GloomyCaveEntertainment <contact@gloomycaveentertainment.com>
2018-07-20 20:04:02 +0200

    Added iPhoneX layout helper to Main Menu and Level Selection + Inventory for first test

---
+++ UP/Assets/Scripts/Utils/XPhoneTransformHelper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XPhoneTransformHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        if (/*UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX && */_parent == null)
+        {
+            RectTransform rt = GetComponent<RectTransform>();
+            rt.offsetMin = new Vector2(_left, _bottom);
+            rt.offsetMax = new Vector2(-_right, _top);
+        }
+        
+    }
+    void Start()
+    {
+        if (/*UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX &&*/ _parent != null)
+        {
+            RectTransform rt = GetComponent<RectTransform>();
+            rt.offsetMin = new Vector2(_left, _bottom);
+            rt.offsetMax = new Vector2(-_right, _top);
+        }
+        //Destroy(this);
+
+    }
+
+    [SerializeField]
+    private float _left, _top, _right, _bottom;
+    [SerializeField]
+    private XPhoneTransformHelper _parent;
+}
